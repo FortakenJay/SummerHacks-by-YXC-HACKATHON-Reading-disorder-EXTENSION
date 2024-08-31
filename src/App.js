6ea9739df7ad635eb336610c19e83e57ee0e6e33
jih bin luo liu <jihbin520@gmail.com>
2024-08-31 01:21:13 -0500

    filing change

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import main from "./components/main";
+
+function App() {
+    return (
+    <>
+    <main>
+      
+    </main>
+    </>
+    );
+}
+
+export default App;
---
+++ src/components/main.js
@@ -0,0 +1,10 @@
+
+function main() {
+    return (
+     <>
+        <h1>hello world</h1>
+     </>
+      );
+    }
+    
+    export default main;
